a1c96ee591811e9be0def88a1b386553b33f3ed2
LUIS HENRIQUE LEAL RIES <98657828049@senacrs.edu.br>
2025-09-02 18:48:44 -0300

    Outras funcionalidades: buscarPorId, atualizar, deletar, buscas

--- main.js
+++ main.js
@@ -15,6 +15,20 @@ function main() {
 
     produtoRepository.inserir(produto1);
     produtoRepository.inserir(produto2);
+    produtoRepository.inserir({nome:"Feijao", categoria:"Alimento", preco:6.7});
+    produtoRepository.inserir({nome:"Coca-cola", categoria:"Bebida", preco:8.9});
+    produtoRepository.inserir({nome:"Detergente", categoria:"Limpeza", preco:2.5});
+
+    console.log(produtoRepository.listar());
+    console.log("Produto [id=2]: ", produtoRepository.buscarPorId(2));
+
+    console.log("Produtos da categoria Alimento", produtoRepository.pesquisarPorCategoria("Alimento"));
+
+    console.log("Produtos que possuem a letra 'a'", produtoRepository.pesquisarPorNomeLike("a"));
+
+    console.log("Produto atualizado ", produtoRepository.atualizar(4, {nome:"Coca-cola", categoria:"Bebida", preco: 8.5, id:4}));
+
+    console.log("Produto removido: ", produtoRepository.deletar(1));
 
     console.log(produtoRepository.listar());
 
